c86a4d4cda796ae980d626bc6c47e668bafdc8a8
Chris Wiseman <wiseman.christopher@gmail.com>
2019-11-07 19:27:08 -0400

    Allow selecting a generation when getting Pokedex ID

--- src/helpers.ts
+++ src/helpers.ts
@@ -11,6 +11,49 @@ export function find<T extends { id: number }>(
   return found.length > 0 ? found[0] : null;
 }
 
-export function randomPokemon(): number {
-  return Math.floor(Math.random() * (809 - 1 + 1)) + 1;
+/**
+ * Returns a random Pokedex ID based on the generation.
+ * Defaults to Gen 1.
+ * @param generation number 1 - 7
+ */
+export function randomPokemon(generation: number = 1): number {
+  let min = 0;
+  let max = 0;
+
+  switch (generation) {
+    case 1:
+      min = 1;
+      max = 151;
+      break;
+    case 2:
+      min = 152;
+      max = 251;
+      break;
+    case 3:
+      min = 252;
+      max = 386;
+      break;
+    case 4:
+      min = 387;
+      max = 493;
+      break;
+    case 5:
+      min = 494;
+      max = 649;
+      break;
+    case 6:
+      min = 650;
+      max = 721;
+      break;
+    case 7:
+      min = 722;
+      max = 809;
+      break;
+    default:
+      min = 1;
+      max = 151;
+      break;
+  }
+
+  return Math.floor(Math.random() * (max - min)) + min;
 }
